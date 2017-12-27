e54806fb6e7515dfbae320050fb758261fe50858
Shivani Mathur <qwellery@gmail.com>
2017-12-27 14:30:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,5 +6,7 @@ def take_a_number(katz_deli, name)
   if katz_deli.empty?
     puts "Welcome, #{name}. You are number 1 in line."
     return katz_deli.push(name)
+  else
+    puts "Welcome, #{name}. You are number #{katz_deli.size + 1} in line."
   end
 end
