4cfff88ca64c3f38aec76b319ea1d7323fb97019
Shivani Mathur <qwellery@gmail.com>
2017-12-27 14:14:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,10 @@ def line(array = [])
   if array.empty?
     return "should say the line is empty"
   else
-    
+    counter = 0
+    array.each do |name|
+      counter += 1
+    end
+    puts "should say the line is empty #{list}"
   end
 end
